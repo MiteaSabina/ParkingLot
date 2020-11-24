82022f9567ec5fb04eba2f5749e578e29a17ec0d
neagoe <neagoe@LAPTOP-NIO644O8>
2020-11-24 12:32:47 +0200

    created Login and Logout

--- src/main/java/com/park/parkinglot/servlet/Login.java
+++ src/main/java/com/park/parkinglot/servlet/Login.java
@@ -5,12 +5,8 @@
  */
 package com.park.parkinglot.servlet;
 
-import com.park.parkinglot.common.UserDetails;
-import com.park.parkinglot.ejb.UserBean;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,11 +17,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author neagoe
  */
-@WebServlet(name = "Users", urlPatterns = {"/Users"})
-public class Users extends HttpServlet {
-    
-    @Inject
-    private UserBean userBean;
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -44,10 +37,10 @@ public class Users extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Users</title>");            
+            out.println("<title>Servlet Login</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Users at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -65,13 +58,7 @@ public class Users extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            //processRequest(request, response);
-        request.setAttribute("activePage", "Users");
-        
-        List<UserDetails> users = userBean.getAllUsers();
-        request.setAttribute("users", users);
-        
-        request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
     }
 
     /**
